29035878dcdeb2e4be5de8c0602f0b33dffd26ea
Calum Dingwall <24cdingwall@gmail.com>
2021-04-23 00:25:33 +0000

    code formatting

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const express = require('express');
 const server = express();
 
 server.all('/', (req, res)=>{
-  res.send('Discord PinBoard<br /><a href="https://discord.com/api/oauth2/authorize?client_id=809803000066867225&permissions=8192&scope=bot">Invite</a>');
+	res.send('Discord PinBoard<br /><a href="https://discord.com/api/oauth2/authorize?client_id=809803000066867225&permissions=8192&scope=bot">Invite</a>');
 })
 
 function keepAlive(){
-  server.listen(3000, ()=>{console.log('Server is Ready!')});
+	server.listen(3000, ()=>{console.log('Server is Ready!')});
 }
 
 module.exports = keepAlive;
