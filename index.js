7cc81559679aea4e8e5bd44dc767e7dda78e491e
Calum Dingwall <24cdingwall@gmail.com>
2021-04-23 00:25:19 +0000

    code formatting

--- index.js
+++ index.js
@@ -11,21 +11,21 @@ client.on('ready', () => {
 });
 
 client.on('channelPinsUpdate', async (channel, time) => {
-  if (time.getTime() === 0) {return} // Time is 0 when the last message is unpinned
+	if (time.getTime() === 0) {return} // Time is 0 when the last message is unpinned
 
 	channel.messages.fetchPinned()
 		.then(messages => {
-      if (!messages) {return}
+			if (!messages) {return}
 
 			let message = messages.last();
 
-      if (message.guild.channels.cache.find(chan => chan.name === 'pinboard')) {
-        var pinboard = message.guild.channels.cache.find(chan => chan.name === 'pinboard');
-      } else {
-        message.channel.send('I can\'t find a channel named `pinboard`! Please create a channel using that name.');
-        message.unpin();
-        return;
-      }
+			if (message.guild.channels.cache.find(chan => chan.name === 'pinboard')) {
+				var pinboard = message.guild.channels.cache.find(chan => chan.name === 'pinboard');
+			} else {
+				message.channel.send('I can\'t find a channel named `pinboard`! Please create a channel using that name.');
+				message.unpin();
+				return;
+			}
 
 			let embed = new Discord.MessageEmbed()
 				.setDescription(`${message.content}\n\n[Jump to message](https://discord.com/channels/${message.guild.id}/${message.channel.id}/${message.id}) in <#${message.channel.id}>`)
@@ -35,25 +35,25 @@ client.on('channelPinsUpdate', async (channel, time) => {
 				embed.setImage(message.attachments.first().url)
 			}
 
-      pinboard.fetchWebhooks()
-        .then(webhooks => {
-          let pinhook = webhooks.find(hook => hook.owner.id === client.user.id);
+			pinboard.fetchWebhooks()
+				.then(webhooks => {
+					let pinhook = webhooks.find(hook => hook.owner.id === client.user.id);
 
-          if (!pinhook) {
-            pinhook = pinboard.createWebhook('PinBoard', {
-              avatar: client.user.displayAvatarURL({dynamic: true}),
-            });
-          }
+					if (!pinhook) {
+						pinhook = pinboard.createWebhook('PinBoard', {
+							avatar: client.user.displayAvatarURL({dynamic: true}),
+						});
+					}
 
-          pinhook.send({
-            username: message.author.username,
-            avatarURL: message.author.displayAvatarURL(),
-            embeds: [embed],
-          });
-          message.unpin({
-            reason: 'Moved to the pinboard'
-          });
-        });
+					pinhook.send({
+						username: message.author.username,
+						avatarURL: message.author.displayAvatarURL(),
+						embeds: [embed],
+					});
+					message.unpin({
+						reason: 'Moved to the pinboard'
+					});
+				});
 		});
 });
 
